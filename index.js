553fae993eb9b414738612d613c20c342789c485
Emmanuel Nwogbo <nerdyemmanuel@gmail.com>
2021-02-05 01:30:49 +0100

    init commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+let url = "";
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
+
+app.get("/url", (req, res) => {
+  res.send({
+    url,
+  });
+});
+
+app.post("/url", (req, res) => {
+  console.log(req.body);
+  url = req.body.url;
+  res.send({
+    message: "url changed",
+  });
+});
+
+app.listen(3000, () => console.log("Gator app listening on port 3000!"));
